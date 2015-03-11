d269e02b62c8fd4c674ab0c6a5df7df9d997d6fa
kolin liu <932100375@qq.com>
2015-03-11 20:03:38 +0800

    更新db.py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,12 +1,3 @@
-#数据库引擎对象：
-class _Engine(object):
-	def __init__(self, connect):
-		self._connect = connect
-	def connect(self):
-		return self._connect
-
-engine = None
-
 #持有数据库连接的上下文对象：
 class _DbCtx(threading.local):
 	def __init__(self):
@@ -28,3 +19,65 @@ class _DbCtx(threading.local):
 		return self.connection.cursor()
 
 _db_ctx = _DbCtx()
+
+#数据库引擎对象：
+class _Engine(object):
+	def __init__(self, connect):
+		self._connect = connect
+	def connect(self):
+		return self._connect
+
+engine = None
+
+
+class _ConnctionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_cleanup = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_cleanup = True
+		return self
+
+	def __exit__(self, exctype, excvalue, traceback):
+		global _db_ctx
+		if self.should_cleanup:
+			_db_ctx.cleanup()
+
+def connection():
+	return _ConnctionCtx()
+
+class _TransactionCtx(object):
+	def __enter__(self):
+		global _db_ctx
+		self.should_close_conn = False
+		if not _db_ctx.is_init():
+			_db_ctx.init()
+			self.should_close_conn = True
+		_db_ctx.transactions = _db_ctx.transactions + 1
+		return self
+
+	def __exit__(self, exctype, excvalue, traceback):
+		global _db_ctx
+		_db_ctx.transactions = _db_ctx.transactions - 1
+		try:
+			if _db_ctx.transactions == 0:
+				if exctype is None:
+					self.commit()
+				else:
+					self.rollback()
+		finally:
+			if self.should_close_conn:
+				_db_ctx.cleanup()
+
+	def commit(self):
+		global _db_ctx
+		try:
+			_db_ctx.connection.commit()
+		except:
+			_db_ctx.connection.rollback()
+			raise
+
+	def rollback(self):
+		global _db_ctx
+		_db_ctx.connection.rollback
