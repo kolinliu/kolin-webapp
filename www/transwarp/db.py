1ecbc202e77b58c05ea629bf84066208224ac770
kolinliu <932100375@qq.com>
2015-03-14 23:39:39 +0800

    update db.py

--- www/transwarp/db.py
+++ www/transwarp/db.py
@@ -1,3 +1,22 @@
+class Dict(dict):
+	def __init__(self, names=(), values=(), **kw):
+		super(Dict, self).__init__(**kw)
+		for k, v in zip(names, values):
+			self[k] = v
+
+	def __getattr__(self, key):
+		try:
+			return self[key]
+		except KeyError:
+			raise AttributeError(r"'Dict' object has no attibute '%s'" % key)
+
+	def __setattr__(self, key, )
+		self[key] = value
+
+
+class DBError(Exception):
+	pass
+
 #持有数据库连接的上下文对象：
 class _DbCtx(threading.local):
 	def __init__(self):
@@ -29,6 +48,20 @@ class _Engine(object):
 
 engine = None
 
+def create_engine(user, password, database, host='127.0.0.1', port=3306, **kw):
+	import mysql.connector
+	global engine
+	if engine is not None:
+		raise DBError('Engine is already initialized.')
+	params = dict(user=user, password = password, database = database, host = host, port = port)
+	defaults = dict(use_unicode=True, charset='utf8', collation='utf8_general_ci', autocommit=False)
+	for k, v in defaults.iteritems():
+		params[k] = kw.pop(k, v)
+	params.update(kw)
+	params['buffered'] = True
+	engine = _Engine(lambda: mysql.connector.connect(**params))
+	logging.info('Init mysql engine <%s> ok.' % hex(id(engine)))
+
 
 class _ConnctionCtx(object):
 	def __enter__(self):
@@ -46,6 +79,46 @@ class _ConnctionCtx(object):
 
 def connection():
 	return _ConnctionCtx()
+
+
+class _LasyConnection(object):
+	def __init__(self):
+		self.connection = connection
+
+	def cursor(self):
+		if self.connection is None:
+			connection = engine.connect()
+			self.connection = connection
+		return self.connection.cursor()
+		
+	def commit(self):
+		self.connection.commit()
+
+	def  rollback(self):
+		self.connection.rollback()
+
+	def cleanup(self):
+		if self.connection:
+			connection = self.connection
+			self.connection = None
+			connection.close()
+
+def with_connection(func):
+	"""
+	Decorator for reuse connection.
+
+	@with_connection
+	def foo(*arg, **kw):
+		f1()
+		f2()
+		f3()
+	"""
+	@functools.wraps(func)
+	def _wrapper(*args, **kw):
+		with _ConnctionCtx():
+			return func(*args, **kw)
+	return _wrapper
+
 
 class _TransactionCtx(object):
 	def __enter__(self):
@@ -81,3 +154,68 @@ class _TransactionCtx(object):
 	def rollback(self):
 		global _db_ctx
 		_db_ctx.connection.rollback
+
+def transaction():
+	return _TransactionCtx()
+
+def with_transaction(func):
+	@functools.wraps(func)
+	def _wrapper(*args, **kw):
+		_start = time.time()
+		with _TransactionCtx():
+			return func(*args, **kw)
+		_profiling(_start)
+	return _wrapper
+
+def  _select(sql, first, *args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?', '%s')
+	logging.info('sql: %s, args: %s' % (sql, args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		if cursor.description:
+			names = [x[0] for x in cursor.description]
+		if first:
+			values = cursor.fetchone()
+			if not values:
+				return None
+			return Dict(names, values)
+		return [Dict(names, x) for x in cursor.fetchall()]
+	finally:
+		if cursor:
+			cursor.close()
+
+@with_connection
+def  select_one(sql, *args):
+	return _select(sql, True, *args)
+
+@with_connection
+def select_int(sql, *args):
+	d = _select(sql, True, *args)
+	if len(d) != 1:
+		raise MultiColumnsError('Expect only one column.')
+	return d.values()[0]
+
+@with_connection
+def select(sql, *args):
+	return _select(sql, False, *args)
+
+@with_connection
+def _update(sql, *args):
+	global _db_ctx
+	cursor = None
+	sql = sql.replace('?', '%s')
+	logging.info('SQL: %s, ARGS: %s' % (sql, args))
+	try:
+		cursor = _db_ctx.connection.cursor()
+		cursor.execute(sql, args)
+		r = cursor.rowcount
+		if _db_ctx.transactions == 0:
+			logging.info('auto commit')
+			_db_ctx.connection.commit()
+		return r
+	finally:
+		if cursor:
+			cursor.close()
